08e849a974271c68a3479669c26c78b14360dd3d
Luis <luismgval@gmail.com>
2022-05-26 14:04:14 +0200

    component control alternativo props ready

--- src/02-components-patterns/hooks/useSoppingCart.ts
+++ src/02-components-patterns/hooks/useSoppingCart.ts
@@ -2,44 +2,27 @@ import { useState } from "react";
 import { Product } from "../interfaces/interfaces";
 
 interface ProductInCart extends Product {
-    count: number;
-  }
+  count: number;
+}
 
 export const useShoppingCart = () => {
+  const [shoppingCart, setShoppingCart] = useState<{
+    [key: string]: ProductInCart;
+  }>({});
 
-    const [shoppingCart, setShoppingCart] = useState<{
-        [key: string]: ProductInCart;
-      }>({});
-    
-      const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
-        console.log({ count });
-    
-        setShoppingCart((oldShoppingCart) => {
-        /*   const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-    
-          if (Math.max(productInCart.count + count, 0) > 0) {
-            productInCart.count += count;
-            return {
-              ...oldShoppingCart,
-              [product.id]: productInCart,
-            };
-          } */
-    
-          //delete product from cart
-    
-        /*   const { [product.id]: toDelete, ...restShoppingCart } = oldShoppingCart;
-          return restShoppingCart; */
-    
-            if (count === 0) {
-            const { [product.id]: toDelete, ...newShoppingCart } = oldShoppingCart;
-            return newShoppingCart;
-          }
-          return { ...oldShoppingCart, [product.id]: { ...product, count } };
-        });
-      };
-    
-      console.log("shpping cart", shoppingCart);
+  const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
+    console.log({ count });
 
-      return { shoppingCart, onProductCountChange };
+    setShoppingCart((oldShoppingCart) => {
+      if (count === 0) {
+        const { [product.id]: toDelete, ...newShoppingCart } = oldShoppingCart;
+        return newShoppingCart;
+      }
+      return { ...oldShoppingCart, [product.id]: { ...product, count } };
+    });
+  };
 
-    }
+  console.log("shpping cart", shoppingCart);
+
+  return { shoppingCart, onProductCountChange };
+};
